432c624f02f457426628148e677b36b143351098
cdmihai <codobanmihai@gmail.com>
2014-01-03 17:02:04 -0800

    added stub method for first AST inferencer call

--- edu.oregonstate.cope.eclipse.replaying/src/edu/illinois/codingtracker/replaying/ASTInferencerFacade.java
+++ edu.oregonstate.cope.eclipse.replaying/src/edu/illinois/codingtracker/replaying/ASTInferencerFacade.java
@@ -1,19 +1,26 @@
 package edu.illinois.codingtracker.replaying;
 
+import edu.illinois.codingtracker.operations.UserOperation;
+import edu.illinois.codingtracker.operations.textchanges.TextChangeOperation;
 import edu.illinois.codingtracker.recording.ast.ASTOperationRecorder;
 
 public class ASTInferencerFacade {
 	private ASTOperationRecorder astRecorder;
 
-	private static class Instance{
+	private static class Instance {
 		public static final ASTInferencerFacade _instance = new ASTInferencerFacade();
 	}
-	
-	public static ASTInferencerFacade getInstance(){
+
+	public static ASTInferencerFacade getInstance() {
 		return Instance._instance;
 	}
-	
+
 	private ASTInferencerFacade() {
 		astRecorder = ASTOperationRecorder.getInstance();
+	}
+
+	public void beforeDocumentChanged(UserOperation currentUserOperation) {
+		if (!(currentUserOperation instanceof TextChangeOperation))
+			return;
 	}
 }
